205fcb2a10b74f9cdaad0e33eb68e140f2358bb1
Shreyash Roy <22052762@kiit.ac.in>
2025-07-14 03:47:29 +0530

    Add files via upload

---
+++ Q1/src/main/java/com/example/webflux/WebfluxGreetApplication.java
@@ -0,0 +1,11 @@
+package com.example.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxGreetApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(WebfluxGreetApplication.class, args);
+    }
+}
